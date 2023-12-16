52eefc1fa029f566c1d57a30a352835e43840d94
Amani Salah <salah.amani2001@gmail.com>
2023-12-16 11:13:06 +0100

    Add structure definition and initialization function for 'BinaryTree'
    - Added typedef struct BinaryTree in BinaryTree.h
    - Included a function prototype for init_BinaryTree in BinaryTree.h.
    - Implemented init_BinaryTree function in BinaryTree.c

---
+++ DataStructures/BinaryTree.c
@@ -0,0 +1,8 @@
+//
+// Created by amani on 16/12/23.
+//
+
+#include "BinaryTree.h"
+void init_BinaryTree(BinaryTree *t){
+    t->root = NULL;
+}
---
+++ DataStructures/BinaryTree.h
@@ -0,0 +1,11 @@
+//
+// Created by amani on 16/12/23.
+//
+
+#ifndef HANGMANGAME_BINARYTREE_H
+#define HANGMANGAME_BINARYTREE_H
+typedef struct BinaryTree{
+    root=NULL;
+}BinaryTree;
+void init_BinaryTree(BinaryTree *t);
+#endif //HANGMANGAME_BINARYTREE_H
