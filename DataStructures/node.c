1288c796a34491404e68ae26b7a8ac7ee5825a1f
Amani Salah <salah.amani2001@gmail.com>
2023-12-16 10:57:52 +0100

    Add structure definition and initialization function for 'node'
    - Added typedef struct node in node.h to define a structure representing a tree node.
    - Included a function prototype for init_node in node.h.
    - Implemented init_node function in node.c to initialize a 'node' structure.

---
+++ DataStructures/node.c
@@ -0,0 +1,10 @@
+//
+// Created by amani on 16/12/23.
+//
+
+#include "node.h"
+
+void init_node (node *n){
+    data='\0';
+    n->left = n->right = NULL;
+}
---
+++ DataStructures/node.h
@@ -0,0 +1,13 @@
+//
+// Created by amani on 16/12/23.
+//
+
+#ifndef HANGMANGAME_NODE_H
+#define HANGMANGAME_NODE_H
+typedef struct node{
+    char data;
+    node* left, *right;
+}node;
+void init_node (node *n );
+
+#endif //HANGMANGAME_NODE_H
