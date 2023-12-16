e2117fd26adf88cd4ce92a28325bc9c290495cbd
Amani Salah <salah.amani2001@gmail.com>
2023-12-16 12:33:15 +0100

    Added Insert function for inserting a new node into the binary search tree

--- DataStructures/BinaryTree.c
+++ DataStructures/BinaryTree.c
@@ -3,6 +3,27 @@
 //
 #include <stddef.h>
 #include "BinaryTree.h"
+#include <stdlib.h>
+#include <stdio.h>
 void init_BinaryTree(BinaryTree *t){
     t->root = NULL;
+}
+
+node* Insert(node* root, char item){
+    if(root==NULL){
+        node *newnode = malloc(sizeof(node));
+        if (newnode == NULL) {
+            // Handle memory allocation failure
+            printf("Memory allocation failed. Unable to insert new node.\n");
+            return root;
+        }
+        init_node(newnode,item);
+        root=newnode;
+    }
+    else if (item<root->data){
+        root->left =Insert(root->left,item);
+    }else{
+        root->right= Insert(root->right,item);
+    }
+    return root;
 }
