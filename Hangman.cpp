b99750d5e690ba3584448171e04ed1fdb8e1519c
Amani Salah <salah.amani2001@gmail.com>
2024-02-13 23:16:48 +0100

    modif

--- Hangman.cpp
+++ Hangman.cpp
@@ -6,10 +6,128 @@
 #include <cstring>
 #include <sstream>
 #include <list>
+#include <vector>
 #include "File_manipulation/dic.h"
+#include <cstdlib>
+// ANSI escape codes for text color
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define RESET "\033[0m"
 #define tab_size 11
 using namespace std;
- char *arr[tab_size] = {"cas","cassette", "ce", "ces","ci","de","des", "don","fas","font","kas" };
+//jdyd
+using namespace std;
+/************************************************* functions  ******************************************************************/
+void display_misses(int misses)
+{
+    if(misses==0)
+    {
+        cout<<"  +---+ \n";
+        cout<<"  |   | \n";
+        cout<<"      | \n";
+        cout<<"      | \n";
+        cout<<"      | \n";
+        cout<<"      | \n";
+        cout<<" ========= \n";
+    }
+    else if(misses==1)
+    {
+        cout<<"  +---+ \n";
+        cout<<"  |   | \n";
+        cout<<"  O   | \n";
+        cout<<"      | \n";
+        cout<<"      | \n";
+        cout<<"      | \n";
+        cout<<" ========= \n";
+    }else if(misses==2)
+    {
+        cout<<"  +---+ \n";
+        cout<<"  |   | \n";
+        cout<<"  O   | \n";
+        cout<<"  |   | \n";
+        cout<<"      | \n";
+        cout<<"      | \n";
+        cout<<" ========= \n";
+    }
+    else if(misses==3)
+    {
+        cout<<"  +---+ \n";
+        cout<<"  |   | \n";
+        cout<<"  O   | \n";
+        cout<<" /|   | \n";
+        cout<<"      | \n";
+        cout<<"      | \n";
+        cout<<" ========= \n";
+    }
+    else if(misses==4){
+        cout<<"  +---+ \n";
+        cout<<"  |   | \n";
+        cout<<"  O   | \n";
+        cout<<" /|\\  | \n";
+        cout<<"      | \n";
+        cout<<"      | \n";
+        cout<<" ========= \n";
+    }
+    else if(misses==5)
+    {
+        cout<<"  +---+ \n";
+        cout<<"  |   | \n";
+        cout<<"  O   | \n";
+        cout<<" /|\\  | \n";
+        cout<<" /    | \n";
+        cout<<"      | \n";
+        cout<<" ========= \n";
+    }
+    else if(misses==6)
+    {
+        cout<<"  +---+ \n";
+        cout<<"  |   | \n";
+        cout<<"  O   | \n";
+        cout<<" /|\\  | \n";
+        cout<<" / \\  | \n";
+        cout<<"      | \n";
+        cout<<" ========= \n";
+    }
+}
+void greet() {
+    // Use color for the greeting
+    cout << "=====================\n";
+    cout << GREEN << "Hangman: The Game\n";
+    cout << "=====================\n";
+    cout << "Instructions: Save your friend from being hanged by guessing the letters in the codeword.\n";
+    cout << RESET;
+}
+void display_status(vector<char> incorrect, string answer)
+{
+    //cout<<"Incorrect Guesses: \n";
+
+   /* for(int i = 0; i<incorrect.size(); i++)
+    {
+        cout<<incorrect[i]<<" ";
+    }*/
+
+    //cout<<"\nCodeword:\n";
+
+    for(int i = 0; i<answer.length(); i++)
+    {
+        cout<<answer[i]<<" ";
+    }
+}
+
+
+void end_game(string answer, string codeword)
+{
+    if(answer==codeword)
+    {
+        cout<<"Hooray! You saved the person from being hanged and earned a medal of honor!\n";
+        cout<<"Congratulations!\n";
+    }
+    else
+    {
+        cout<<"On no! The man is hanged!\n";
+    }
+}
 
 void get_word_from_user(int size,Node* root,char* guessed_word  );
 
@@ -17,7 +135,7 @@ void afficherMots(string s,list<string>& l,  Node*  a)
 {
     if (a != NULL)
     {
-        if (a->data != '\0') 
+        if (a->data != '\0')
         {
             if (a->left != NULL) //ajouter caractere a mot
             {
@@ -33,7 +151,7 @@ void afficherMots(string s,list<string>& l,  Node*  a)
         {   l.push_back(s);
             cout<<s<<endl; // l'occurence du mot dans le dictionnaire
             //if (a->left != NULL)
-              //  afficherMots(s,l, a->left);
+            //  afficherMots(s,l, a->left);
         }
     }
 }
@@ -59,209 +177,57 @@ void traverseNodes(stringstream& ss, string padding, string pointer, Node* node,
     }
 }
 
-// Function to print the binary tree
-void print(Node* root) {
-    stringstream ss;
-    traverseNodes(ss, "", "", root, false );
-    cout << ss.str();
-}
 
 
-Node* remove_word(Node* root , char* word,int len )
-{
-    BinaryTree t; 
- if ( root == NULL) 
-    return root; 
- 
- Node* tmp= root ; 
- Node* test; 
- int j =0; 
- for ( int i = 0 ; i< len ; ++i )
- {
-      
-     tmp = t.Search(tmp,word[i]); 
-	 printf ("%d , %c \n",i,tmp->data);  
-	tmp = tmp->left; 
-	 
-	 //printf ("iil , %c \n",tmp->data);  
-	//printf ("tmp- %c \n" , tmp->parent->data); 
-	if ( tmp->data == '\0' && i == len-1 ) 
-	{
-	printf ("tmp- %c \n" , tmp->parent->data); 
-		while ( tmp->parent != NULL && j <= len -1  )
-		{
-			if ( tmp->parent->data != '\0' ) 
-			{
-			printf ("d %c \n", tmp->data); 
-			if ( tmp->right != NULL) 
-			{
-				if ( tmp->data== '\0') 
-				{
-					printf("removing /0 \n");  
-					test= tmp; 
-					tmp->parent->left=tmp->right;	
-					j++; 
-					tmp = tmp->parent; 
-					printf("aa %c \n", tmp->left->data);  
-					free(test); 
-					test=NULL;
-				}
-				else
-				{
-					tmp = tmp->parent; 
-				j++;} 
-			}else 
-			{
-				printf ("removing %c \n", tmp->data);
-				test= tmp ; 
-                tmp = tmp->parent; 
-				j++; 
-                free(test); 
-                test=NULL; 
-				tmp->right = NULL; 
-			}
-			}else {
-				printf ("removing %c \n", tmp->data);
-				test= tmp ; 
-                tmp = tmp->parent; 
-				j++;
-                free(test); 
-                test=NULL; 
-				tmp->right = NULL; 
-				return root; 	}}	 }}
-return root; 
-}
 int check_user_input(Node* root,char* guessed_word,char user_in, int len, int *arr)
 {
     BinaryTree t;
     Node *tmp = root;
-    bool flag =false ; 
-	int j=0; 
-if (root==NULL)
-	return j ; 
-for ( int i=0;i<len;i++)
-{
-	tmp = t.Search(tmp,guessed_word[i]);
-    if (tmp->data == user_in )
-	{
-		arr[j]=i;
-		j++;
-		flag=true;
-	}
-		tmp=tmp->left;
-	if (tmp->data == '0'&& i==len)
+    bool flag =false ;
+    int j=0;
+    if (root==NULL)
+        return j ;
+    for ( int i=0;i<len;i++)
     {
-      return j;   
+        tmp = t.Search(tmp,guessed_word[i]);
+        if (tmp->data == user_in )
+        {
+            arr[j]=i;
+            j++;
+            flag=true;
+        }
+        tmp=tmp->left;
+        if (tmp->data == '0'&& i==len)
+        {
+            return j;
+        }
+
+
     }
-	
-
+    return j ;
 }
-return j ;
-}
-using namespace std;
-int main() {
-    //const char* inputFilePath = "/home/moktar/Hangman_Game/File_manipulation/dic.txt";
-     //const char* outputFilePath = "/home/moktar/Hangman_Game/File_manipulation/result.txt";
 
-    //sortFile(outputFilePath,inputFilePath);
-    //readFile(outputFilePath);
-/* 
-    //test 5edmet mokhtar
-    char *arr[3] = { "ces", "ce", "don" };
-    char *s = "ci";
-    char *s2 = "cas";
-    //char *ptrs = s2;
-
-
-
-    int j = 0;
-    //tree = new BinaryTree();
-    //init_BinaryTree(&tree);
-    tree.root= tree.insert_word(tree.root, s2, j);
-    tree.root = tree.insert_word(tree.root, arr[1], j);
-    tree.root = tree.insert_word(tree.root, arr[0], j);
-    tree.root = tree.insert_word(tree.root,s, j);
-    tree.root = tree.insert_word(tree.root, "de", j);
-    tree.root = tree.insert_word(tree.root, "des", j);
-    tree.root = tree.insert_word(tree.root,arr[2] , j);
-
-    tree.Inorder(tree.root);
-    printf("\n");
-    printf ("%c \n" , tree.root->data);
-    */
-	    BinaryTree tree;
-	int j=0; 
-    for (int i = 0; i<tab_size;i++)
-	{
-    tree.root = tree.insert_word(tree.root, arr[i], j);
-	}  
-   // tree.Inorder(tree.root);
-       print(tree.root);
-
-	printf("\n");
-   tree.Find_Word(tree.root, arr[5],strlen("de"));	
-	//tree.root =  remove_word(tree.root, "cas",strlen("cas"));  
-
-   //tree.root =  remove_word(tree.root, arr[4],strlen(arr[4]));  
-    //  tree.root =  remove_word(tree.root, arr[3],strlen(arr[3]));  
-   //tree.root =  remove_word(tree.root, arr[2],strlen(arr[2]));  
-
-    printf("\n");
-    print(tree.root);
-	printf("\n");
-	Node* sub = tree.get_sbtree_by_num(tree.root,0);
-	list<string> l ; 
-    string s ;
-	afficherMots(s,l, tree.root);
-	cout<<tree.root->data<<endl;
-	cout<<"---------------------\n"; 
-	for ( string str : l )
-	{
-		cout <<str<<endl;} 
-	
-	/*
-	tree.root = tree.Find_Word(tree.root, arr[0], strlen(arr[0]));
-	printf("-------\n");
-    
-	tree.root = tree.Find_Word(tree.root, arr[2], strlen(arr[2]));
-	printf("-------\n");
-    
-	tree.root = tree.Find_Word(tree.root,"fon", strlen("font"));
-	printf("-------\n");
-
-    printf ("number of subtree, %d \n",tree.get_number_subtree(tree.root)); 
-
-    Node* sub = tree.get_sbtree_by_num(tree.root,0);
-	Node* sub1 = tree.get_subtree_by_char(tree.root,'k'); 
-	printf ("sub data : %c \n ", sub->data); 
-
-	printf ("sub size : %d \n", tree.sizeTree(sub));
-	printf("%c \n",sub1->data);;
-    get_word_from_user(strlen(arr[1]), sub,arr[1]);
-	printf("-------\n");
-	*/
-}
 
 void  set_up_word(char*  word_toguess, int len_str)
 {
-	 // +1 for the null terminator
-   for (int i = 0; i < len_str; i++) {
+    // +1 for the null terminator
+    for (int i = 0; i < len_str; i++) {
         word_toguess[i] = '*';
     }
     word_toguess[len_str] = '\0'; // Null-terminate the string
 }
-void update_guessed_word(char* word_toguess, int *arr, char c, int len_arr)  
-	{
+void update_guessed_word(char* word_toguess, int *arr, char c, int len_arr)
+{
     // Initialize the array with asterisks
-     // Update guessed letters in the array
+    // Update guessed letters in the array
     for (int i = 0; i < len_arr; i++) {
-        
-            word_toguess[arr[i]] = c;
-        }
-    
+
+        word_toguess[arr[i]] = c;
+    }
+
 
     printf("%s\n", word_toguess);
-	
+
 }
 
 
@@ -270,35 +236,89 @@ void get_word_from_user(int size,Node* root,char* guessed_word )
     BinaryTree t;
     printf("guessed word %s \n", guessed_word);
     t.Find_Word(root, guessed_word, size);
-	Node* tmp=root;
-	int state=0 ; 
+    Node* tmp=root;
+    int state=0 ;
     char c;
-	int arr[10];
-	int i=0,j=0;
-	char word_guessed[size+ 1];
-	printf("enter the first char from the word you want to guess \n");
-	set_up_word(word_guessed,size);
-	printf("%s\n",word_guessed);
-	while (i<size)
-	{
-        scanf(" %c", &c); 	
-		state = check_user_input(tmp,guessed_word, c,size,arr);
-		if (state >0) 
-		{ 
-			printf ("found  %c \n",c);
-			update_guessed_word(word_guessed,arr,c, state);
-			i+=state; 
-			}
-		else {
-			printf ("not found  %c \n",c);
+    int arr[10];
+    int i=0,j=0;
+    char word_guessed[size+ 1];
+    int misses=0;
+    printf("enter the first char from the word you want to guess \n");
+    set_up_word(word_guessed,size);
+    printf("%s\n",word_guessed);
+    while (i<size && misses<6)
+    {
+        scanf(" %c", &c);
+        state = check_user_input(tmp,guessed_word, c,size,arr);
+        if (state >0)
+        {
+            printf ("found  %c \n",c);
+            update_guessed_word(word_guessed,arr,c, state);
+            i+=state;
+        }
+        else {
+            misses++;
+            display_misses(misses);
+            printf ("not found  %c \n",c);
 
 
 
+        }
     }
-	}
 
 
-   
 
-return ;
+
+    return ;
 }
+
+
+int main() {
+    greet();
+
+    string codeword = "codingcleverly";
+    string answer = "______________";
+
+
+    int misses = 0;
+    vector<char> incorrect;
+    bool guess = false;
+    char letter;
+
+
+    while(answer!=codeword && misses < 7)
+    {
+        system("clear");
+        display_misses(misses);
+        display_status(incorrect, answer);
+
+        cout<<"\n\nPlease enter your guess: ";
+        cin>>letter;
+
+        for(int i = 0; i<codeword.length(); i++)
+        {
+            if(letter==codeword[i])
+            {
+                answer[i] = letter;
+                guess = true;
+            }
+        }
+        if(guess)
+        {
+            cout<<"\nCorrect!\n";
+        }else
+        { system("clear");
+            //cout<<"\nIncorrect! Another portion of the person has been drawn.\n";
+            incorrect.push_back(letter);
+            misses++;
+
+        }
+        guess = false;
+    }
+
+    end_game(answer, codeword);
+    return 0;
+}
+
+
+
